19b38af85075a773741696ad951baf2662da1f7e
Pramod <single.core.developers@gmail.com>
2023-08-25 10:36:32 +0530

    header commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a5954f4bd267f5b"; }
+/******/ 		__webpack_require__.h = function() { return "69bd1590d0af9b20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
